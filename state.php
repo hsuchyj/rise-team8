6326f2847c7a92e19f70f792666cf9df32b5ff62
unknown <hsuchyj@udel.edu>
2019-09-28 08:30:43 -0400

    yoyo

--- state.php
+++ state.php
@@ -1,8 +1,16 @@
 <?php
-echo $_REQUEST["state"];
+include "cleardb.php";
 
-if ($_REQUEST["state"] == "Texas")
+$result = $conn->query("select * from state_leg where state = '".$_REQUEST["state"] ."'");
+$row = $result->fetch_row();
+
+if ($mysqli->connect_errno) {
+    printf("Connect failed: %s\n", $mysqli->connect_error);
+    exit();
+}
+
+for($i = 0; $i < 4; $i++)
 {
-	echo "<br>yeehaw";
+	echo $row[$i] . "<br>";
 }
 ?>
