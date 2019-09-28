b7d5f4f8f879a03d7c1eb4cf1a0f99671d76442d
unknown <hsuchyj@udel.edu>
2019-09-28 08:22:59 -0400

    yoyo

--- state.php
+++ state.php
@@ -1,7 +1,7 @@
 <?php
 include "cleardb.php";
 
-$result = $conn->query("select * from state_leg where state =".$_REQUEST["state"]);
+$result = $conn->query("select * from state_leg where state = '".$_REQUEST["state"] ."'");
 $row = $result->fetch_row();
 
 for($i = 0; $i < 4; $i++)
