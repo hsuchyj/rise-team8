134760ce16fc0b616c1d8440a568f6cefcb96e20
unknown <hsuchyj@udel.edu>
2019-09-28 07:22:26 -0400

    yoyo

--- cleardb.php
+++ cleardb.php
@@ -1,11 +1,11 @@
 <?php
 
-$url = parse_url(getenv("mysql://b3ba3197c1465a:d29468f2@us-cdbr-iron-east-02.cleardb.net/heroku_bf15bd8453ae177?reconnect=true"));
+//$url = parse_url(getenv("mysql://b3ba3197c1465a:d29468f2@us-cdbr-iron-east-02.cleardb.net/heroku_bf15bd8453ae177?reconnect=true"));
 
-$server = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$db = substr($url["path"], 1);
+$server = "us-cdbr-iron-east-02.cleardb.net";
+$username = "b3ba3197c1465a";
+$password = "d29468f2";
+$db = "heroku_bf15bd8453ae177";
 
 echo $server . " " . $username . " " . $password . " " . $db;
 $conn = new mysqli($server, $username, $password, $db);
