92839d4a4e1656424a211611d66dab1ea10d12a5
unknown <hsuchyj@udel.edu>
2019-09-28 09:23:59 -0400

    yoyo

--- state.php
+++ state.php
@@ -1,3 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+.card {
+  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+  transition: 0.3s;
+  width: 60%;
+  height: 60%;
+  border-radius: 24px;
+}
+
+.card:hover {
+  box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+}
+
+img {
+  border-radius: 8px 8px 0 0;
+}
+
+.container {
+  padding: 100px 16px;
+}
+</style>
+</head>
+<body>
+<div class="card">
+  <div class="container">
 <?php
 include "cleardb.php";
 
@@ -20,13 +49,12 @@ $labels = array("state","convenes","adjourns","carryover","period");
 $i =0;
 foreach($row as $col)
 {
-	echo $labels[$i] . ": " . $col . "<br>";
+	echo "<h4><b>" .$labels[$i] . ": " . $col . "</b></h4><br>";
 	$i++;
 }
-/*
-for($i = 0; $i < 5; $i++)
-{
-	echo $labels[$i] . ": " $row[$labels[$i]] . "<br>";
-}
-*/
+
 ?>
+</div>
+</div>
+</body>
+</html>
