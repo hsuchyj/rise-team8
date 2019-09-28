5d184501b9b3e49c0058e2654e4782a9490c24c1
unknown <hsuchyj@udel.edu>
2019-09-28 03:20:17 -0400

    yoyo

--- Rise Home Page.php
+++ Rise Home Page.php
@@ -5,10 +5,10 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title> Rise Home Page </title>
-<link href="css/singlePageTemplate.css" rel="stylesheet" type="text/css">
+<link href="/css/singlePageTemplate.css" rel="stylesheet" type="text/css">
 
 <script>var __adobewebfontsappname__="dreamweaver"</script>
-<script src="http://use.edgefonts.net/source-sans-pro:n2:default.js" type="text/javascript"></script>
+<script src="https://use.edgefonts.net/source-sans-pro:n2:default.js" type="text/javascript"></script>
 
 </head>
 <body>
