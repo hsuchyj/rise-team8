54ccf3c93bd70ec6f701be167eb526e300743ede
unknown <hsuchyj@udel.edu>
2019-09-28 08:33:06 -0400

    yoyo

--- state.php
+++ state.php
@@ -4,13 +4,15 @@ include "cleardb.php";
 $result = $conn->query("select * from state_leg where state = '".$_REQUEST["state"] ."'");
 $row = $result->fetch_row();
 
-if ($mysqli->connect_errno) {
+if ($mysqli->connect_errno) 
+{
     printf("Connect failed: %s\n", $mysqli->connect_error);
     exit();
 }
 
-for($i = 0; $i < 4; $i++)
+$labels = array("state","convenes","adjourns","carryover","period");
+for($i = 0; $i < 5; $i++)
 {
-	echo $row[$i] . "<br>";
+	echo $labels[$i] . ": " $row[$i] . "<br>";
 }
 ?>
