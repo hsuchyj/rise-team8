49901bb611f0cacb29a40799530c12822fc4d1ef
unknown <hsuchyj@udel.edu>
2019-09-28 07:18:14 -0400

    yoyo

--- cleardb.php
+++ cleardb.php
@@ -1,6 +1,6 @@
 <?php
 
-$url = parse_url(getenv("us-cdbr-iron-east-02.cleardb.net"));
+$url = parse_url(getenv("mysql://b3ba3197c1465a:d29468f2@us-cdbr-iron-east-02.cleardb.net/heroku_bf15bd8453ae177?reconnect=true"));
 
 $server = $url["host"];
 $username = $url["user"];
